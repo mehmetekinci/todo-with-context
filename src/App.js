1c252730718426c098839913f82ef22cc19f3646
mehmetekinci <mehmetekinci1989@gmail.com>
2021-07-15 21:24:39 +0200

    added context api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React from 'react';
+import { CssBaseline, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Form } from './Form';
+import { Global } from './Global';
+
 import './App.css';
 
+const useStyles = makeStyles({
+  container: { height: '80vh' },
+});
 function App() {
-  return <div className='App'></div>;
+  const classes = useStyles();
+
+  // Note: within the Root function we can return any Component (not just SomeComponent, but also a Router for instance)
+  return (
+    <Global
+      Root={() => {
+        return (
+          <div className='App'>
+            <>
+              <CssBaseline />
+              <Container maxWidth='sm' className={classes.container}>
+                <Form />
+              </Container>
+            </>
+          </div>
+        );
+      }}
+    />
+  );
 }
 
 export default App;
